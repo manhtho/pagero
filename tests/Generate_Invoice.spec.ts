9c6c6ae022812425b3c4bccd5b91db569572fc16
Tony <manh.nguyen@o2.cz>
2025-05-17 13:05:46 +0200

    Add Download

--- tests/Generate_Invoice.spec.ts
+++ tests/Generate_Invoice.spec.ts
@@ -1,6 +1,15 @@
 import { test } from '../fixtures/BasePage'
+import * as path from 'node:path'
 
-test('Generate Invoice', async ({page}) => {
-  await page.goto('http://localhost:3000/invoice');
-  await new Promise(r => setTimeout(r, 15000));
+test('Generate Invoice', async ({ page }) => {
+  await page.goto('http://localhost:3000/invoice')
+  await new Promise((r) => setTimeout(r, 5000))
+
+  const pdfPath = path.join('./test-results', 'invoice.pdf')
+  await page.pdf({
+    path: pdfPath,
+    format: 'A4',
+    printBackground: true,
+  })
+  console.log('PDF saved to:', pdfPath)
 })
