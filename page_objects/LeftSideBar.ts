06e28a1d1a7c2454b13a47538f451d90b43dd720
Tony <manh.nguyen@o2.cz>
2026-03-28 19:43:19 +0100

    Pridal jsem tlacitko button

--- page_objects/LeftSideBar.ts
+++ page_objects/LeftSideBar.ts
@@ -2,9 +2,11 @@ import type { Locator, Page } from '@playwright/test'
 
 export class LeftSideBar {
   private readonly leftSideBar: Locator
+  readonly button: Locator
 
   constructor(public readonly page: Page) {
     this.leftSideBar = page.locator('.leftSidebar')
+    this.button = this.leftSideBar.getByRole('button')
   }
 
   async navigateTo(menuItem: string) {
