609bf32966005c812ce708e7fa46f3dac75b11d8
Tony <manh.nguyen@o2.cz>
2026-03-28 19:49:12 +0100

    pridaj jsem dalsi button

--- page_objects/LeftSideBar.ts
+++ page_objects/LeftSideBar.ts
@@ -3,10 +3,12 @@ import type { Locator, Page } from '@playwright/test'
 export class LeftSideBar {
   private readonly leftSideBar: Locator
   readonly button: Locator
+  readonly anotherButton: Locator
 
   constructor(public readonly page: Page) {
     this.leftSideBar = page.locator('.leftSidebar')
-    this.button = this.leftSideBar.getByRole('button')
+    this.button = this.leftSideBar.getByRole('button').first()
+    this.anotherButton = this.leftSideBar.getByRole('button').nth(1)
   }
 
   async navigateTo(menuItem: string) {
